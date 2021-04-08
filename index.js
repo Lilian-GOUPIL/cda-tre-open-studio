0a82a554480c2b5db4d69321fc9440076d5cf78e
Lilian GOUPIL <liliangoupil.pro@gmail.com>
2021-04-08 14:55:21 +0200

    feat(everything): The whole project.
    Added everything for the project to work.

---
+++ index.js
@@ -0,0 +1,17 @@
+anime({
+    targets: '.container-fluid',
+    keyframes: [
+        {opacity: 1}
+    ],
+    duration: 1000,
+    easing: 'linear'
+});
+
+anime({
+    targets: '.card',
+    keyframes: [
+        {opacity: 1}
+    ],
+    duration: 2000,
+    easing: 'linear'
+});
